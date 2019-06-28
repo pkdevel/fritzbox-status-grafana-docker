274f99db546f53ec8e2b718ad0cba316a7e1f0ae
Philipp Kolb <p.kolb@me.com>
2019-06-28 22:24:41 +0200

    created docker file for grafana with fritz box status dashboard

---
+++ docker-run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker run --name fb-grafana \
+        -p 3000:3000 \
+        -v $(pwd)/conf/collectd.conf:/etc/collectd/collectd.conf.d/collectd.conf \
+        -v $(pwd)/conf/influxdb.conf:/etc/influxdb/influxdb.conf \
+        -v $(pwd)/conf/grafana-datasource-influxdb.yaml:/etc/grafana/provisioning/datasources/grafana-datasource-influxdb.yaml \
+        -v $(pwd)/conf/grafana-dashboard-fb-status.yaml:/etc/grafana/provisioning/dashboards/grafana-dashboard-fb-status.yaml \
+        -v $(pwd)/conf/grafana-dashboard-fb-status.json:/etc/grafana/provisioning/dashboards/fb-status/grafana-dashboard-fb-status.json \
+        -e "GF_SECURITY_ADMIN_PASSWORD=" \
+        fb-grafana
